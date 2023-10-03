77b267068c5bcd02c980021653a6a698dce1eac4
Guillermo Guevara <guillermoguevara887@gmail.com>
2023-10-02 19:48:23 -0700

    final commit

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-console.log("hello");
+let scoreLeft = document.getElementById("score-left");
+let scoreRight = document.getElementById("score-right");
+var scoreHome = 0;
+var scoreGuest = 0;
+
+
+
+function addOne() {
+    scoreHome = scoreHome + 1;
+    scoreLeft.textContent = +scoreHome;
+
+}
+function addTwo() {
+    scoreHome = scoreHome + 2;
+    scoreLeft.textContent = +scoreHome;
+
+}
+function addThree() {
+    scoreHome = scoreHome + 3;
+    scoreLeft.textContent = +scoreHome;
+
+}
+function addOneGuest() {
+    scoreGuest = scoreGuest + 1;
+    scoreRight.textContent = +scoreGuest;
+
+}
+function addTwoGuest() {
+    scoreGuest = scoreGuest + 2;
+    scoreRight.textContent = +scoreGuest;
+
+}
+function addThreeGuest() {
+    scoreGuest = scoreGuest + 3;
+    scoreRight.textContent = +scoreGuest;
+
+}
+
+function reset() {
+    scoreHome = 0;
+    scoreGuest = 0;
+    scoreLeft.textContent = +scoreGuest;
+    scoreRight.textContent = +scoreGuest;
+
+}
